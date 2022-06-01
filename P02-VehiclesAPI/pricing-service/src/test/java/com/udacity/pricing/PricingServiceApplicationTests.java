d7676966d7e49a3ffb9de9e7c80545d3ae951f82
GEORGIANA BLUE <GEORGIANABLUE@gmail.com>
2022-06-01 00:10:59 -0400

    [06-01-22] Additional test for the overall Pricing Service Application

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,33 @@
 package com.udacity.pricing;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
+	
+	@Autowired
+    private MockMvc mvc;
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void getPrice() throws Exception {
+		mvc.perform(
+				get("/services/price").param("vehicleId", "1"))
+					.andExpect(status().isOk());
 	}
 
 }
